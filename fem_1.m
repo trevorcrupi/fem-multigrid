d8494affd6a26df967d14236ecc8722edcb7e6c4
Trevor <tcrupi@purdue.edu>
2020-06-22 11:19:52 -0400

    Cleaned up variables and spaces/indents, removed unnecessary files

--- fem_1.m
+++ fem_1.m
@@ -7,10 +7,15 @@ geometryFromEdges(model, g);
 pdemesh(p, e, t);
  
 f = @(x, y) ((pi^2-1)/pi).*(cos(pi.*y));
-numOfTriangles = size(t, 2);
-count = 1;
-bCount = 1;
-for i = 1 : numOfTriangles
+
+
+triangles    = size(t, 2);
+globalAIndex = 1;
+globalBindex = 1;
+
+triquadNodes = 8;
+
+for i = 1 : triangles
   col = t(1:3, i);
   
   basisMatrix = [
@@ -25,39 +30,38 @@ for i = 1 : numOfTriangles
  
   basis = [ linsolve(basisMatrix, e1) linsolve(basisMatrix, e2) linsolve(basisMatrix, e3) ];
   
-  % Integral value of basis i and basis j
-  quadritureMatrix = [ p(1, col(1)) p(2, col(1)); p(1, col(2)) p(2, col(2)); p(1, col(3)) p(2, col(3)) ];
-  
-  [ X, Y, Wx, Wy ] = triquad(8, quadritureMatrix);
-  
-  localAIntegrand = @(basis1, basis2, x, y) (basis1(1).*basis2(1) + basis1(2).*basis2(2)) + (basis1(1).*x + basis1(2).*y + basis1(3)).*(basis2(1).*x + basis2(2).*y + basis2(3));
+  % Integral value of basis i and basis j  
+  [ X, Y, Wx, Wy ] = triquad(triquadNodes, basisMatrix(:, 1:2));
   
   localA = zeros(3, 3);
+  A_ij = @(basis1, basis2, x, y) (basis1(1).*basis2(1) + basis1(2).*basis2(2)) + (basis1(1).*x + basis1(2).*y + basis1(3)).*(basis2(1).*x + basis2(2).*y + basis2(3));
+  b_i = @(basis, x, y) f(x,y)*(basis(1).*x + basis(2).*y + basis(3));
+  
   for k = 1:3 
       for j = 1:3
-          localA(k, j) = Wx' * localAIntegrand(basis(:, k), basis(:, j), X, Y) * Wy;
+          localA(k, j) = Wx' * A_ij(basis(:, k), basis(:, j), X, Y) * Wy;
       end 
   end
   
   for m = 1:3
       for n = 1:3
-          globalNodeRowMatrix(count) = col(m);
-          globalNodeColMatrix(count) = col(n);
-          entryMatrix(count) = localA(m, n);
-          count = count+1;
+          globalSparseRow(globalAIndex) = col(m);
+          globalSparseCol(globalAIndex) = col(n);
+          globalSparseEntry(globalAIndex) = localA(m, n);
+          globalAIndex = globalAIndex+1;
       end 
   end 
   
-  bIntegrand = @(basis, x, y) f(x,y)*(basis(1).*x + basis(2).*y + basis(3));
   for q=1:3
-    bRowNode(bCount) = col(q);
-    bColumnNode(bCount) = 1;
-    bColumnEntry(bCount) = Wx' * bIntegrand(basis(:, q), X, Y) * Wy;
-    bCount = bCount+1;
+    bSparseRow(globalBindex) = col(q);
+    bSparseCol(globalBindex) = 1;
+    bSparseEntry(globalBindex) = Wx' * b_i(basis(:, q), X, Y) * Wy;
+    globalBindex = globalBindex+1;
   end  
 end
 
-globalAMatrix = sparse(globalNodeRowMatrix, globalNodeColMatrix, entryMatrix, size(p, 2), size(p, 2));
-globalBMatrix = sparse(bRowNode, bColumnNode, bColumnEntry, size(p, 2), 1);
+globalAMatrix = sparse(globalSparseRow, globalSparseCol, globalSparseEntry, size(p, 2), size(p, 2));
+globalBMatrix = sparse(bSparseRow, bSparseCol, bSparseEntry, size(p, 2), 1);
 
 u_h = globalAMatrix\globalBMatrix;
+u_h
