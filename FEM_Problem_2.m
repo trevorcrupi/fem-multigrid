0a33a4d5abaaa8d2f3807161d233a8e346706d36
Trevor <tcrupi@purdue.edu>
2020-07-07 14:59:13 -0400

    exterminated bugs

--- FEM_Problem_2.m
+++ FEM_Problem_2.m
@@ -1,38 +1,30 @@
 clear all
 load Yoni/PETForYoni/PETForYoni1.mat
-load new_ele1.mat
+load Yoni/newEle/new_ele1.mat
 
 edge = getEdgeMatrix(p,t);
 
+% ourF = ;
+curlRZK = @() [
+    ];
+
+psiVector = @(basis,r,z) [ (basis(2)/k).*r - (basis(1)/k).*r.*z
+                            0
+                           (basis(3)/k).*r + (basis(1)/k).*r.^2 ];
+                       
+phiVector = @(basis,r,z) [ - (basis(3)/k) - (basis(1)/k).*r - (basis(2)/k).*z
+                           basis(1).*r + basis(2).*z + basis(3)
+                                                               0];
+
+                                                           
+                                                           
 numOfTriangles = size(t,2);
  for i = 1:numOfTriangles
-     
-      rowVector = new_ele(i, 1:3); % Vector with points of the triangle
-     
-%     % Points of triangle
-%     p1 = columnVector(1);
-%     p2 = columnVector(2);
-%     p3 = columnVector(3);
-         
-%     % Edge directions? Which point first? Correct?
 
-%      solvePsi = [ p(1,p1).*p(2,p2)-p(2,p1).*p(1,p2)   p(1,p2)-p(1,p1)   p(2,p2)-p(2,p1)            %[ x1y2-y1x2  x2-x1  y2-y1
-%                   p(1,p2).*p(2,p3)-p(2,p2).*p(1,p3)   p(1,p3)-p(1,p2)   p(2,p3)-p(2,p2)            %  x1y2-y1x2  x2-x1  y2-y1
-%                   p(1,p1).*p(2,p3)-p(2,p1).*p(1,p3)   p(1,p3)-p(1,p1)   p(2,p3)-p(2,p1) ];         %  x1y2-y1x2  x2-x1  y2-y1 ]
-
-    edge1 = rowVector(1);
-    edge2 = rowVector(2);
-    edge3 = rowVector(3);
-    % x1y2-y1x2  x2-x1  y2-y1
-    solvePsi = [ p(1,edge(edge1,1)).*p(2,edge(edge1,2))-p(2,edge(edge1,1)).*p(1,edge(edge1,2))   p(1,edge(edge1,2))-p(1,edge(edge1,1))   p(2,edge(edge1,2))-p(2,edge(edge1,1))
-                 p(1,edge(edge2,1)).*p(2,edge(edge2,2))-p(2,edge(edge2,1)).*p(1,edge(edge2,2))   p(1,edge(edge2,2))-p(1,edge(edge2,1))   p(2,edge(edge2,2))-p(2,edge(edge2,1))
-                 p(1,edge(edge3,1)).*p(2,edge(edge3,2))-p(2,edge(edge3,1)).*p(1,edge(edge3,2))   p(1,edge(edge3,2))-p(1,edge(edge3,1))   p(2,edge(edge3,2))-p(2,edge(edge3,1)) ];
+    columnVector = t(1:3, i); % Vector with points of the triangle
+    rowVector = new_ele(i, 1:3); % Vector with points of the triangle
     
-    
-    i1 = [1;0;0];
-    i2 = [0;1;0];
-    i3 = [0;0;1];
- 
-    % Matrix with columns of Phi Coefficients
-    localPsiCoeffs = [ linsolve(solvePsi, i1) linsolve(solvePsi, i2) linsolve(solvePsi, i3) ];
+    localPhiCoeffs = getLocalPhiCoeffs(p,columnVector);
+    localPsiCoeffs = getLocalPsiCoeffs(p,rowVector,edge);
+   
  end
---
+++ localAEntry.m
@@ -0,0 +1,12 @@
+
+
+function [localAEntry] = localAEntry(basis1, basis2, k, i, j)
+    
+    if i > 3
+        
+    end
+    if j > 3
+        
+    end
+
+end
