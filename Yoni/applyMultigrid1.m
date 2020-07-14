82d70196a75eb5f512834a43031b4c1f2b4c61e9
Yonah Moise <yonahmoise@Yonahs-MacBook.local>
2020-07-14 12:34:26 -0400

    update

---
+++ Yoni/applyMultigrid1.m
@@ -0,0 +1,10 @@
+function multigrid1 = applyMultigrid1(globalA, globalB, height)
+
+    for i = 1:size(p,2)
+        Q_j = zeros(size(Z_i), height)   ; %Z_i?????
+        A_j = zeros(size(Z_i), size(Z_i));
+        
+        
+    end
+
+end
